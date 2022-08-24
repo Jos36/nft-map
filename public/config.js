12cf8eaac86bcb6da4f79e01f5ce52b70cadda8e
Jos36 <youssefmohamedsabry36@gmail.com>
2022-08-24 16:33:55 +0200

    updating contract

--- public/config.js
+++ public/config.js
@@ -1,11 +1,14 @@
 export const config = {
   contract: {
-    contract_address: "0x931258fb3C737bE7722934A884D561d8cE61a7C0",
-    mint_price: "40000000000000000",
+    contract_address: "0xdC4284009767cba461e7473Ec5FB6013F795f68d",
+    one_mint_price: "180000000000000000",
+    six_mint_price: "3540000000000000000",
+    twelve_mint_price: "13470000000000000000",
+    twentyfour_mint_price: "51090000000000000000",
     mint_function: "mint",
     sale_live: "paused",
   },
   requirements: {
-    chain_id: 4,
+    chain_id: 1,
   },
 };
